40421c3dfd18b5ea563870a37db240e476b75c32
Sergiusz <sergiusz.sanetra@gmail.com>
2023-02-19 23:51:24 +0100

    update

--- commands/test.command.js
+++ commands/test.command.js
@@ -1,14 +1,20 @@
 const { SlashCommandBuilder } = require('discord.js')
-
+const { take_and_save_image } = require("../webcam")
+//
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('test')
-		.setDescription('test!'),
+		.setName('photo')
+		.setDescription('Take photo'),
     //
 	async execute(interaction) {
-		await interaction.reply('test!')
+		interaction.reply({ content: `Taking photo...!`, ephemeral: true })
 		//
-		const { test } = require("../webcam")
-		test()
+		const zdj = await take_and_save_image()
+		//
+		if(zdj) {
+			interaction.editReply({ content: "Successfully shot the photo!", files: [`${__dirname}/../image.png`]})
+		} else {
+			interaction.editReply({ content: "Failed while taking the photo!" })
+		}
 	}
 }
--- webcam.js
+++ webcam.js
@@ -1,17 +1,28 @@
 const NodeWebcam = require("node-webcam")
 const options = require("./options.json")
 const Webcam = NodeWebcam.create(options)
+const Jimp = require("jimp")
 //
-function test() {
-    Webcam.capture("test_picture", function( err, data ) {
-        // Jimp.read("test_picture.png", (err, lenna) => {
-        //     if (err) throw err;
-        //     lenna.write("image.jpg")
-        //     setTimeout(() => {
-        //         hook.send({content: `hello`, files: [`${__dirname}/image.jpg`] }).then(msg => { null })
-        //     }, 500);
-        // });
-    } );
+function take_and_save_image() {
+    return new Promise(async (res, err) => {
+        Webcam.capture("image", (err1, data) => {
+            if(err1) {
+                console.error(err1)
+                return res(undefined)
+            }
+            //
+            Jimp.read("image.png", (err2, image) => {
+                if(err2) {
+                    console.error(err2)
+                    return res(undefined)
+                }
+                image.writeAsync("image.png").then(() => {
+                    res(true)
+                })
+            });
+            //
+        })
+    })
 }
 
-module.exports = { test }
+module.exports = { take_and_save_image }
